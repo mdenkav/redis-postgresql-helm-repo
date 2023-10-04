d12a20d63f833d1230bab9f3bb66086f449a7a06
Deniz <mdenizkavruk@gmail.com>
2023-10-04 22:05:49 +0000

    dosya edit 3

--- all_infra_installation.sh
+++ all_infra_installation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /home/ec2-user/
+
 apt update -y & apt upgrade -y
 
 apt install git-all -y
@@ -28,3 +30,25 @@ cd /home/ec2-user/deniztest.git
 
 helm create redis
 helm create postgresql
+touch README.md
+touch all_infra_installation.sh
+
+git add .
+git config --global user.email "mdenizkavruk@gmail.com"
+git config --global user.name "Deniz"
+git commit -m "firstcommit"
+git status
+
+#Bu aşamada repodan helm deploy yapılabilir.Bu test çalışmasında bitnami hazır helm reposundan imaj çekilerek doğrudan kurulum yapılmıştır.
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo ls
+helm install my-redis bitnami/redis
+helm install my-redis bitnami/postgresql
+helm ls
+
+#Eğer git reposundaki helm chartlar tam düzenlenmiş olsaydı aşağıdaki gibi kurulum yapılabilecektir.
+#helm install deniz-postgresql postgresql
+#helm install deniz-redis redis
+
+#veya helm reposu sadece helm chartlardan oluşuyorsa tek adımda kurulum yapılabilir.
+#helm install deniz-deployment deniztest.git
