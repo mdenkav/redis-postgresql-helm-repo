45e7350d5c91a1c34bb0798ccd5c40f35b9b735e
Deniz <mdenizkavruk@gmail.com>
2023-10-04 13:28:48 +0000

    tum dosyalar ilk olusturuldu

---
+++ all_infra_installation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+apt update -y & apt upgrade -y
+
+apt install git-all -y
+
+git --version
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start --force
+
+kubectl get nodes
+
+helm ls
+
+git init deniztest.git
+
+cd /home/ec2-user/deniztest.git
+
+helm create redis
+helm create postgresql
