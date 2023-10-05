6abed98c9a12a4c8111afb4a0257d47bf7fe6b89
Deniz <mdenizkavruk@gmail.com>
2023-10-05 09:46:58 +0000

    dosya düzenleme 3

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# deniztest
-deniztest
+# deniztest isimli, deployment amaçlı git reposudur.
+Kurulum git paketi Redis ve Postgresql Helm Chart dosyaları, altyapı gereksinimlerini uçtan uca kuracak .sh dosyası ve açıklamanın olduğu README dosyasından oluşmaktadır.
+
+Ubuntu sunucularda hazır olarak çalıştırılabilir.
+
+Repoyu lokale aldıktan sonra içinde uçtan uca gereklilikleri kuran .sh dosyası çalıştırılır.
+.sh dosyası Ubuntu sunuya sırasıyla Update yapar, git,helm,docker,kubectl,minikube kurulumlarını yapar, redis ve postgresql helm deploylarını yapar, git sürecini yönetir ve pod,node kontrollerini yapar.
--- all_infra_installation.sh
+++ all_infra_installation.sh
@@ -3,31 +3,35 @@
 #/home/ec2-user/ dizin değişikliği benim test ortamı özelindedir.
 cd /home/ec2-user/
 
-echo "Sunucu Update Başlıyor..."  
+echo "---Sunucu Update Başlıyor...---"  
 apt update -y & apt upgrade -y
-echo "Sunucu Update Tamamlandı."
+echo "---Sunucu Update Tamamlandı.---"
 
-echo "Gerekli paket kurulumları ve GIT kurulumu başlıyor..."
-apt install git-all curl wget htop -y
+echo "------------------------------------------------------"
+echo "---GIT kurulumu başlıyor...---"
+apt install git-all -y
 git --version
-echo "Gerekli paket kurulumları ve GIT kurulumu tamamlandı."
+echo "---Gerekli paket kurulumları ve GIT kurulumu tamamlandı.---"
 
-echo "Helm paket kurulumu başlıyor..."
+echo "------------------------------------------------------"
+echo "---Helm paket kurulumu başlıyor...---"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-echo "Helm paket kurulumu tamamlandı."
+echo "---Helm paket kurulumu tamamlandı.---"
 
 sleep 2
-echo "Docker Servis kurulumu başlıyor..."
+echo "------------------------------------------------------"
+echo "---Docker Servis kurulumu başlıyor...---"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 systemctl start docker & systemctl enable docker
-echo "Docker Servis kurulumu tamamlandı."
+echo "---Docker Servis kurulumu tamamlandı.---"
 docker ps
 
 sleep 2
-echo "Minikube ve kubectl kurulumu başlıyor..."
+echo "------------------------------------------------------"
+echo "---Minikube ve kubectl kurulumu başlıyor...---"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
@@ -35,21 +39,24 @@ kubectl version --client
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube start --force
-echo "Minikube ve kubectl kurulumu tamamlandı."
+echo "---Minikube ve kubectl kurulumu tamamlandı.---"
 
 kubectl get nodes
 sleep 2
 helm ls
 
-echo "GIT ve HELM redis ve postgresql deployları başlıyor..." 
+echo "------------------------------------------------------"
+echo "---GIT ve HELM redis ve postgresql deployları başlıyor...---" 
 git init deniztest
 
-cd /home/ec2-user/deniztest.git
-
+#bu adımlar (grup sonu adımına kadar olan) manuel ilk kurulum sonrasında silinebilir.
+cd /home/ec2-user/deniztest
 helm create redis
 helm create postgresql
 touch README.md
 touch all_infra_installation.sh
+chmod 750 all_infra_installation.sh
+#grup sonu.
 
 git add .
 git config --global user.email "mdenizkavruk@gmail.com"
@@ -64,8 +71,11 @@ helm install my-redis bitnami/redis
 helm install my-postgresql bitnami/postgresql
 echo "GIT ve HELM redis ve postgresql deployları tamamlandı."
 sleep 2
+echo "------------------------------------------------------"
 helm ls
 kubectl get pods
+echo "TAMAMLANDI"
+echo "------------------------------------------------------"
 
 #Eğer git reposundaki helm chartlar tam düzenlenmiş olsaydı aşağıdaki gibi kurulum yapılabilecektir.
 #helm install deniz-postgresql postgresql
