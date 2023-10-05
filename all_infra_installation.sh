ee222d1bb878a36157cfd1b382a4e7ed1f0e068a
Deniz <mdenizkavruk@gmail.com>
2023-10-05 09:17:22 +0000

    sh dosya edit 3

--- all_infra_installation.sh
+++ all_infra_installation.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
+#/home/ec2-user/ dizin değişikliği benim test ortamı özelindedir.
 cd /home/ec2-user/
 
+echo "Sunucu Update Başlıyor..."  
 apt update -y & apt upgrade -y
+echo "Sunucu Update Tamamlandı."
 
-apt install git-all -y
-
+echo "Gerekli paket kurulumları ve GIT kurulumu başlıyor..."
+apt install git-all curl wget htop -y
 git --version
+echo "Gerekli paket kurulumları ve GIT kurulumu tamamlandı."
 
+echo "Helm paket kurulumu başlıyor..."
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+echo "Helm paket kurulumu tamamlandı."
 
+sleep 2
+echo "Docker Servis kurulumu başlıyor..."
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 systemctl start docker & systemctl enable docker
+echo "Docker Servis kurulumu tamamlandı."
 docker ps
 
+sleep 2
+echo "Minikube ve kubectl kurulumu başlıyor..."
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
@@ -24,12 +35,14 @@ kubectl version --client
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube start --force
+echo "Minikube ve kubectl kurulumu tamamlandı."
 
 kubectl get nodes
-
+sleep 2
 helm ls
 
-git init deniztest.git
+echo "GIT ve HELM redis ve postgresql deployları başlıyor..." 
+git init deniztest
 
 cd /home/ec2-user/deniztest.git
 
@@ -48,8 +61,11 @@ git status
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo ls
 helm install my-redis bitnami/redis
-helm install my-redis bitnami/postgresql
+helm install my-postgresql bitnami/postgresql
+echo "GIT ve HELM redis ve postgresql deployları tamamlandı."
+sleep 2
 helm ls
+kubectl get pods
 
 #Eğer git reposundaki helm chartlar tam düzenlenmiş olsaydı aşağıdaki gibi kurulum yapılabilecektir.
 #helm install deniz-postgresql postgresql
